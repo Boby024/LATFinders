5a5bf95a095056019c4cd36a9b386557b7c60af6
Nikolas Gur <stabilergriller@gmail.com>
2023-01-20 20:31:23 +0100

    scrape categories for all courses at once

--- web harvesting/studyCheckCategoryScraper.py
+++ web harvesting/studyCheckCategoryScraper.py
@@ -8,34 +8,33 @@ if __name__ == '__main__':
     courses = json.load(open('courses.json'))["courses"]
 
     for course in courses:
-        if course["id"] in range(1, 2001):
-            cid = course["id"]
-            link = course["identityLink"]
-            category = None
-            subCategory = None
-            subSubCategory = None
-            print("Currently scraping course with ID " + str(cid))
+        cid = course["id"]
+        link = course["identityLink"]
+        category = None
+        subCategory = None
+        subSubCategory = None
+        print("Currently scraping course with ID " + str(cid))
 
-            review_page = requests.get(link, headers={'User-agent': 'your bot 0.1'})
-            review_soup = BeautifulSoup(review_page.content, "html.parser")
-            ol = review_soup.find("ol", class_="breadcrumb")
-            if ol:
-                lis = ol.find_all("li")
-                if not (len(lis) == 6):
-                    print("Warning!")
-                    print("something is out of the ordinary on " + link)
-                else:
-                    category = lis[2].find("span").text
-                    subCategory = lis[3].find("span").text
-                    subSubCategory = lis[4].find("span").text
+        review_page = requests.get(link, headers={'User-agent': 'your bot 0.1'})
+        review_soup = BeautifulSoup(review_page.content, "html.parser")
+        ol = review_soup.find("ol", class_="breadcrumb")
+        if ol:
+            lis = ol.find_all("li")
+            if not (len(lis) == 6):
+                print("Warning!")
+                print("something is out of the ordinary on " + link)
+            else:
+                category = lis[2].find("span").text
+                subCategory = lis[3].find("span").text
+                subSubCategory = lis[4].find("span").text
 
-            categories.append({
-                'course_id': cid,
-                'course_link': link,
-                'category': category,
-                'subCategory': subCategory,
-                'subSubCategory': subSubCategory
-            })
+        categories.append({
+            'course_id': cid,
+            'course_link': link,
+            'category': category,
+            'subCategory': subCategory,
+            'subSubCategory': subSubCategory
+        })
 
     with open("categories.json", "w") as outfile:
         outfile.write(json.dumps({"categories": categories}, indent=2, ensure_ascii=False))
