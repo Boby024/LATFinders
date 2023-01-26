67545147abeb942542d6c5ac9883770d2429f9f4
Mahdyar <mahdyarsafarian@gmail.com>
2023-01-26 20:37:56 +0100

    Adding the new template

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,72 +1,47 @@
-import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { AppRoutes } from './app.routing';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import {RouterModule, RouterOutlet, Routes} from "@angular/router";
-import {HttpClientModule} from "@angular/common/http";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {PlotlyModule} from "angular-plotly.js";
-import * as PlotlyJS from 'plotly.js-dist-min';
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {ReactiveFormsModule} from "@angular/forms";
-import {MatInputModule} from "@angular/material/input";
-import {MatListModule} from "@angular/material/list";
-import { AnalyticComponent } from './analytic/analytic.component';
-import {MatSelectModule} from "@angular/material/select";
-import {MatButtonModule} from '@angular/material/button';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import { NavigationbarComponent } from './navigationbar/navigationbar.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { UniversityComponent } from './university/university.component';
-import { UniNumberOfRatingsComponent } from './charts/uni-number-of-ratings/uni-number-of-ratings.component';
-import { CoursesPageComponent } from './courses-page/courses-page.component';
-import { CoursesWithCompareModeComponent } from './charts/courses-with-compare-mode/courses-with-compare-mode.component';
 
-PlotlyModule.plotlyjs = PlotlyJS;
-const routes: Routes = [
-  { path: '', component: UniversityComponent },
-  { path: 'login', component: LoginComponent },
-  { path:'course', component: CoursesPageComponent}
-];
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FullComponent } from './layouts/full/full.component';
+import { AppHeaderComponent } from './layouts/full/header/header.component';
+import { AppSidebarComponent } from './layouts/full/sidebar/sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DemoMaterialModule } from './demo-material-module';
+
+import { SharedModule } from './shared/shared.module';
+import { SpinnerComponent } from './shared/spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    LoginComponent,
-    AnalyticComponent,
-    NavigationbarComponent,
-    UniversityComponent,
-    UniNumberOfRatingsComponent,
-    CoursesPageComponent,
-    CoursesWithCompareModeComponent,
+    FullComponent,
+    AppHeaderComponent,
+    SpinnerComponent,
+    AppSidebarComponent
   ],
   imports: [
     BrowserModule,
-    RouterOutlet,
-    RouterModule.forRoot(routes),
-    HttpClientModule,
     BrowserAnimationsModule,
-    PlotlyModule,
-    MatFormFieldModule,
-    ReactiveFormsModule,
-    MatInputModule,
-    MatListModule,
-    MatSelectModule,
-    MatButtonModule,
-    MatSnackBarModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatIconModule
+    DemoMaterialModule,
+    FormsModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    SharedModule,
+    RouterModule.forRoot(AppRoutes, {enableTracing: true})
   ],
-  exports: [RouterModule],
-  providers: [],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: PathLocationStrategy
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
